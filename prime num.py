1b5388e96f32f9274c43f9acbb3c2e70629e9238
punithsabitha <sabithadm36@gmail.com>
2025-12-02 21:48:39 +0530

    Add files via upload

---
+++ prime num.py
@@ -0,0 +1,13 @@
+from  math import sqrt
+number = int(input("Enter your number \n"))
+if number>1:
+    for i in range(2,(sqrt(number))+1):
+        if(number%1==0):
+         print(number, " is not prime") 
+        break
+
+    else:
+     print(number, "is prime")
+
+else:
+ print(number, "is not prime")
